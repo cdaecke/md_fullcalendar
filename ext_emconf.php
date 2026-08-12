fcf1ccac6480e0b63f0f5267d3091026ea858015
chris <typo3@mediadreams.org>
2026-08-12 15:19:51 +0200

    [!!!][TASK] update version constraints for TYPO3 and ext:calendarize

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,8 +23,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '5.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
-            'calendarize' => '13.0.0-14.99.99',
+            'typo3' => '13.4.31-14.3.99',
+            'calendarize' => '15.0.0-15.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
